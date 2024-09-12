50a55c93a6e4eed96a35997bc3e6da6a9ba005ba
Tommy Duc Anh Thai <S78245@BS-HUSUM.DE>
2024-09-12 10:59:55 +0200

    add: views

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,8 @@ from . import views
 app_name="account"
 urlpatterns = [
     path('', views.index, name='index' ),
+    path('login/', views.login_user, name='login_user' ),
+    path('register/', views.register_user, name='register_user' ),
+
+    path('logout/', views.logout_user, name='logout_user' ),
 ]
--- account/views.py
+++ account/views.py
@@ -8,3 +8,43 @@ from django.urls import reverse
 
 def index(request):
     return render(request, 'account/base.html')
+
+
+def login_user(request):
+    L_Form = AuthenticationForm()
+
+    if request.method == 'POST':
+        L_Form = AuthenticationForm(data=request.POST)
+
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect(reverse('account:index'))
+
+    return render(request, 'account/login.html', {'L_Form': L_Form})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def register_user(request):
+    R_Form = UserCreationForm()
+
+    if request.method == 'POST':
+        # print(request.POST)
+
+        R_Form = UserCreationForm(request.POST)
+        if R_Form.is_valid():
+            R_Form.save()
+
+            name = R_Form.cleaned_data['username']
+            password = R_Form.cleaned_data['password1']
+            user = authenticate(request, username=name, password=password)
+            if user:
+                login(request, user)
+                return redirect(reverse('account:index'))
+
+    return render(request, 'account/register.html', {'R_Form': R_Form})
