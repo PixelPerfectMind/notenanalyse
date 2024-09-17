5bd3ac50b2b245158a7916c422b194a255ab210f
Jonas <jonas.praast@web.de>
2024-09-17 11:25:54 +0200

    FIX resource loading
    Resources weren't loaded correctly
    Landing page url didn't worked correctly

--- notenanalyse/urls.py
+++ notenanalyse/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
     path('account/', include('account.urls')),
     path('login/', views.login_user, name='login' ),
     path('register/', views.register_user, name='register' ),
