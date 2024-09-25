3c81985dba787049d1ba322697138f9c5134904b
Jonas <jonas.praast@web.de>
2024-09-25 19:56:55 +0200

    FIX color mode switch

--- static/scripts/ColorMode.js
+++ static/scripts/ColorMode.js
@@ -4,42 +4,39 @@ const darkModeButton = document.getElementById("DarkModeButton");
 const lightModeButton = document.getElementById("LightModeButton");
 const highContrastButton = document.getElementById("HighContrastButton");
 const mediumContrastButton = document.getElementById("MediumContrastButton");
-const defaultColorModeButton = document.getElementById("DefaultColorModeButton");
+const defaultContrastButton = document.getElementById("DefaultContrastButton");
 
 function ToggleDarkMode() {
   pageBody.classList.remove("light")
   pageBody.classList.add("dark");
   darkModeButton.classList.remove('outlined-button');
   lightModeButton.classList.add('outlined-button');
-
-  Save();
+  SetCookie("color_mode", "dark");
 }
+
 function ToggleLightMode() {
   pageBody.classList.remove("dark")
   pageBody.classList.add("light");
   darkModeButton.classList.add('outlined-button');
   lightModeButton.classList.remove('outlined-button');
-
-  Save();
+  SetCookie("color_mode", "light");
 }
 
 function ToggleHighContrast() {
   pageBody.classList.add("hc");
   pageBody.classList.remove("mc")
   mediumContrastButton.classList.add('outlined-button');
-  defaultColorModeButton.classList.add('outlined-button');
+  defaultContrastButton.classList.add('outlined-button');
   highContrastButton.classList.remove('outlined-button');
-
-  Save();
+  SetCookie("contrast_mode", "high");
 }
 function ToggleDefaultContrast() {
   pageBody.classList.remove("hc")
   pageBody.classList.remove("mc")
   highContrastButton.classList.add('outlined-button');
   mediumContrastButton.classList.add('outlined-button');
-  defaultColorModeButton.classList.remove('outlined-button');
-
-  Save();
+  defaultContrastButton.classList.remove('outlined-button');
+  SetCookie("contrast_mode", "default");
 }
 
 function ToggleMediumContrast() {
@@ -47,15 +44,13 @@ function ToggleMediumContrast() {
   pageBody.classList.add("mc");
   highContrastButton.classList.add('outlined-button');
   mediumContrastButton.classList.remove('outlined-button');
-  defaultColorModeButton.classList.add('outlined-button');
-
-  Save();
+  defaultContrastButton.classList.add('outlined-button');
+  SetCookie("contrast_mode", "medium");
 }
 
-function Save() {
-  if (pageBody.classList.contains('dark')) SetCookie("color_mode", "dark");
-  else SetCookie("color_mode", "light");
-
-  if (pageBody.classList.contains('hc')) SetCookie("contrast_mode", "hc");
-  if (pageBody.classList.contains('mc')) SetCookie("contrast_mode", "mc");
-}
+// Apply settings
+if(GetCookie("color_mode") === "light") ToggleLightMode()
+if(GetCookie("color_mode") === "dark") ToggleDarkMode()
+if(GetCookie("contrast_mode") === "high") ToggleHighContrast()
+if(GetCookie("contrast_mode") === "medium") ToggleMediumContrast()
+if(GetCookie("contrast_mode") === "default") ToggleDefaultContrast()
